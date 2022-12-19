91ff30947a7d144c34814f16a633063659fcf86b
Jkawala <91265409+Jkawala@users.noreply.github.com>
2022-12-19 10:31:53 +0000

    Add files via upload

---
+++ Encrypt/Encrypt/Form2.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Encrypt
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.quickScanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mailAttachmentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.protectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.speedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.newSessionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutEncryptoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.supportInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tipsAndTricksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gettingStartedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(197, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 58);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Main Menu bar";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(180)))), ((int)(((byte)(219)))));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.mailAttachmentsToolStripMenuItem,
+            this.protectionToolStripMenuItem,
+            this.speedToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.settingToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuNew,
+            this.quickScanToolStripMenuItem,
+            this.newSessionToolStripMenuItem,
+            this.menuExit});
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.homeToolStripMenuItem.Text = "Home ";
+            // 
+            // menuNew
+            // 
+            this.menuNew.Image = ((System.Drawing.Image)(resources.GetObject("menuNew.Image")));
+            this.menuNew.Name = "menuNew";
+            this.menuNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.menuNew.Size = new System.Drawing.Size(216, 26);
+            this.menuNew.Text = "Homepage";
+            // 
+            // quickScanToolStripMenuItem
+            // 
+            this.quickScanToolStripMenuItem.Name = "quickScanToolStripMenuItem";
+            this.quickScanToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.quickScanToolStripMenuItem.Text = "Quick Scan";
+            // 
+            // mailAttachmentsToolStripMenuItem
+            // 
+            this.mailAttachmentsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.findToolStripMenuItem,
+            this.searchToolStripMenuItem});
+            this.mailAttachmentsToolStripMenuItem.Name = "mailAttachmentsToolStripMenuItem";
+            this.mailAttachmentsToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.mailAttachmentsToolStripMenuItem.Text = "System ";
+            this.mailAttachmentsToolStripMenuItem.Click += new System.EventHandler(this.mailAttachmentsToolStripMenuItem_Click);
+            // 
+            // protectionToolStripMenuItem
+            // 
+            this.protectionToolStripMenuItem.Name = "protectionToolStripMenuItem";
+            this.protectionToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+            this.protectionToolStripMenuItem.Text = "Protection";
+            // 
+            // speedToolStripMenuItem
+            // 
+            this.speedToolStripMenuItem.Name = "speedToolStripMenuItem";
+            this.speedToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.speedToolStripMenuItem.Text = "Applictions";
+            this.speedToolStripMenuItem.Click += new System.EventHandler(this.speedToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.settingToolStripMenuItem.Text = "Setting ";
+            // 
+            // menuExit
+            // 
+            this.menuExit.Image = ((System.Drawing.Image)(resources.GetObject("menuExit.Image")));
+            this.menuExit.Name = "menuExit";
+            this.menuExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.menuExit.Size = new System.Drawing.Size(216, 26);
+            this.menuExit.Text = "Exit";
+            this.menuExit.Click += new System.EventHandler(this.menuExit_Click);
+            // 
+            // newSessionToolStripMenuItem
+            // 
+            this.newSessionToolStripMenuItem.Name = "newSessionToolStripMenuItem";
+            this.newSessionToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.newSessionToolStripMenuItem.Text = "New session";
+            // 
+            // findToolStripMenuItem
+            // 
+            this.findToolStripMenuItem.Name = "findToolStripMenuItem";
+            this.findToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.findToolStripMenuItem.Text = "Find ";
+            // 
+            // searchToolStripMenuItem
+            // 
+            this.searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            this.searchToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.searchToolStripMenuItem.Text = "Search";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutEncryptoToolStripMenuItem,
+            this.supportInfoToolStripMenuItem,
+            this.tipsAndTricksToolStripMenuItem,
+            this.gettingStartedToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutEncryptoToolStripMenuItem
+            // 
+            this.aboutEncryptoToolStripMenuItem.Name = "aboutEncryptoToolStripMenuItem";
+            this.aboutEncryptoToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.aboutEncryptoToolStripMenuItem.Text = "About Encrypto";
+            // 
+            // supportInfoToolStripMenuItem
+            // 
+            this.supportInfoToolStripMenuItem.Name = "supportInfoToolStripMenuItem";
+            this.supportInfoToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.supportInfoToolStripMenuItem.Text = "Support Info";
+            // 
+            // tipsAndTricksToolStripMenuItem
+            // 
+            this.tipsAndTricksToolStripMenuItem.Name = "tipsAndTricksToolStripMenuItem";
+            this.tipsAndTricksToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.tipsAndTricksToolStripMenuItem.Text = "Tips and Tricks ";
+            // 
+            // gettingStartedToolStripMenuItem
+            // 
+            this.gettingStartedToolStripMenuItem.Name = "gettingStartedToolStripMenuItem";
+            this.gettingStartedToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.gettingStartedToolStripMenuItem.Text = "Getting Started ";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(221)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form2";
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mailAttachmentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem protectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem speedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuNew;
+        private System.Windows.Forms.ToolStripMenuItem quickScanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuExit;
+        private System.Windows.Forms.ToolStripMenuItem newSessionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutEncryptoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem supportInfoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tipsAndTricksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gettingStartedToolStripMenuItem;
+    }
+}
